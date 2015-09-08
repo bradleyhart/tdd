8ffb868cc36f9a0308c046c8cdc08188c1afca16
Bradley <bradley@zangle.co.uk>
2015-09-08 21:01:43 +0100

    removing code

---
+++ src/test/java/org/bradleyhart/training/LineTruncatorTest.java
@@ -0,0 +1,10 @@
+package org.bradleyhart.training;
+
+/*
+ * Requirements:
+ * 1. A block of text should get truncated to a new line when the line length is reached
+ * 2. A new word should not be split
+ */
+public class LineTruncatorTest {
+
+}

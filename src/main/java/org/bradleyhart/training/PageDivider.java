48b61d94fbf7e0c7f897c9a4690cd29b907af855
Bradley Hart <bhart@wiley.com>
2015-09-09 15:03:13 +0100

    passed second test

--- src/main/java/org/bradleyhart/training/PageDivider.java
+++ src/main/java/org/bradleyhart/training/PageDivider.java
@@ -5,14 +5,36 @@ import java.util.List;
 
 public class PageDivider {
 
-    public PageDivider(int lineLimit) {
+    private int lineLimit;
+    private LineTruncator lineTruncator;
 
+    public PageDivider(int lineLimit, LineTruncator lineTruncator) {
+        this.lineLimit = lineLimit;
+        this.lineTruncator = lineTruncator;
     }
 
     public List<String> dividePages(String text) {
-        return new ArrayList<String>(){{
-            add("on the first page");
-        }};
+        String truncated = lineTruncator.truncate(text);
+
+        List<String> pages = new ArrayList<String>();
+        StringBuilder page = new StringBuilder();
+
+        String[] lines = truncated.split("\n");
+
+        for (int index = 0; index < lines.length; index++) {
+            page.append(lines[index]);
+
+            if((index + 1) % lineLimit == 0){
+                pages.add(page.toString());
+                page = new StringBuilder();
+            } else if (index != (lines.length - 1)) {
+                page.append('\n');
+            }
+        }
+
+        pages.add(page.toString());
+
+        return pages;
     }
 
 }
--- src/test/java/org/bradleyhart/training/PageDividerTest.java
+++ src/test/java/org/bradleyhart/training/PageDividerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import java.util.List;
 
+import static org.bradleyhart.training.LineLimit.lineLimit;
+import static org.bradleyhart.training.LineTruncator.lineTruncator;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
@@ -12,16 +14,16 @@ public class PageDividerTest {
 
     @Test
     public void shouldNotDivideForHigherLineLimit(){
-        List<String> pages = new PageDivider(100).dividePages("on the first page");
+        List<String> pages = new PageDivider(100, lineTruncator(lineLimit(25))).dividePages("on the first page");
         assertThat(pages.size(), is(1));
         assertThat(pages.get(0), is(equalTo("on the first page")));
     }
 
     @Test
     public void shouldNotDivideAcrossTwoPages(){
-        List<String> pages = new PageDivider(2).dividePages("some text jim");
-        assertThat(pages.size(), is(1));
-        assertThat(pages.get(0), is(equalTo("some\ntext jim")));
+        List<String> pages = new PageDivider(2, lineTruncator(lineLimit(5))).dividePages("some text jim");
+        assertThat(pages.size(), is(2));
+        assertThat(pages.get(0), is(equalTo("some\ntext")));
         assertThat(pages.get(1), is(equalTo("jim")));
     }
 

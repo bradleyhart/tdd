e9dc3547121d39ebca524d4757487e40ca0b0961
Bradley Hart <bhart@wiley.com>
2015-09-09 12:36:33 +0100

    refactor

--- src/main/java/org/bradleyhart/training/PageDivider.java
+++ src/main/java/org/bradleyhart/training/PageDivider.java
@@ -15,25 +15,32 @@ public class PageDivider {
     }
 
     public List<Page> dividePages(String text) {
-        String truncatedText = lineTruncator.truncate(text);
-        String[] lines = truncatedText.split("\n");
+        String[] lines = toLines(text);
 
         List<Page> pages = new ArrayList<Page>();
+        Page currentPage = new Page();
 
-        Page page = new Page();
         for (int index = 0; index < lines.length; index++) {
-            page.addLine(lines[index]);
+            currentPage.addLine(lines[index]);
 
             if(linesPerPage.hasReachedLimit(index)){
-                pages.add(page);
-                page = new Page();
-            } else if(index != (lines.length - 1)) {
-                page.newLine();
+                pages.add(currentPage);
+                currentPage = new Page();
+            } else if(isNotLastLine(lines, index)) {
+                currentPage.newLine();
             }
         }
 
-        pages.add(page);
+        pages.add(currentPage);
 
         return pages;
+    }
+
+    private boolean isNotLastLine(String[] lines, int index) {
+        return index != (lines.length - 1);
+    }
+
+    private String[] toLines(String text) {
+        return lineTruncator.truncate(text).split("\n");
     }
 }

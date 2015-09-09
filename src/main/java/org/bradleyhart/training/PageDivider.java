69ed7c6831e3b08b8790980793cec05e395906b9
Bradley Hart <bhart@wiley.com>
2015-09-09 14:34:12 +0100

    added failing test

---
+++ src/main/java/org/bradleyhart/training/PageDivider.java
@@ -0,0 +1,16 @@
+package org.bradleyhart.training;
+
+import java.util.List;
+
+public class PageDivider {
+
+    public PageDivider(int lineLimit) {
+
+    }
+
+    public List<String> dividePages(String s) {
+
+        return null;
+    }
+
+}
